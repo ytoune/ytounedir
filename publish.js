63d9af3019ca394be4012921c8cfa2c2c359f4c6
toune <ytoune@users.noreply.github.com>
2018-10-07 12:45:57 +0900

    gh-pagesを直に呼ぶ

--- publish.js
+++
@@ -1,5 +0,0 @@
-
-const path = require('path')
-const ghPages = require('gh-pages')
-
-ghPages.publish(path.resolve(__dirname, 'dist'))
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const App = () => (
 	<div>
-		<h1>test</h1>
-		<p>hoge</p>
+		<h1>hoge</h1>
+		<p>fugafuga</p>
 	</div>
 )
